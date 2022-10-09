81fc307b8341a9333d243a23f7717eb482685fa7
Aymen Berbache <aymenberbache21@gmail.com>
2022-10-09 17:49:34 +0100

    Refine Thing Model

--- things/models.py
+++ things/models.py
@@ -1,11 +1,15 @@
+from enum import unique
+from wsgiref.validate import validator
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Creation of model Thing which represent a table in the db.
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
-    
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.CharField(unique=False, blank=True, max_length=120)
+    quantity = models.IntegerField(default=0, unique=False, validator=[MinValueValidator(0), MaxValueValidator(100)])
+
 
 
 
